67ac0d39ec5ffb7910f981e0a55258dbb09fde38
Tomz899 <tomek.nowak@aol.pl>
2022-06-27 20:41:55 +0000

    Added model tests

--- pizza/tests/test_models.py
+++ pizza/tests/test_models.py
@@ -21,17 +21,22 @@ class PizzaMenuModelTest(TestCase):
 
 class OrderModelTest(TestCase):
     def setUp(self):
+        # Creating pizza product
         self.pizza1 = PizzaMenu.objects.create(
             name="pizza1",
             description="pizza1 description",
             ingredients="aaa1",
             price=10,
         )
+
+        # Creating customer
         self.user1 = User.objects.create_user("test1", "test1@testcompany.com", "pass1")
 
+        # Instances of PizzaMenu and User test objects
         obj = self.pizza1
         test_user = self.user1
 
+        # Creating order
         self.order1 = Order.objects.create(
             product=obj,
             customer=test_user,
@@ -42,4 +47,8 @@ class OrderModelTest(TestCase):
         )
 
     def test_create_order(self):
-        self.assertEqual(self.order1.product, "pizza1")
+        self.assertEqual(self.order1.product.name, "pizza1")
+
+    def test_get_item_cost(self):
+        total_cost = self.order1.quantity * self.order1.product.price
+        self.assertEqual(total_cost, 20)
